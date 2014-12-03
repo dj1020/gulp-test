7e28c8cbe82e1a47ebb57364a9fbea9623709bfb
Ken.Lin <ken.lin@cloud-interactive.com>
2014-12-03 15:10:31 +0800

    add uglify plugin and add task for scripts, watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function () {
-    console.log('Hello, World');
+gulp.task('scripts', function () {
+    gulp.src('js/*')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
 });
+
+gulp.task('styles', function () {
+    console.log('runs styles');
+});
+
+gulp.task('watch', function () {
+    gulp.watch('js/*.js', ['scripts']);
+});
+
+gulp.task('default', ['scripts', 'styles']);
 
