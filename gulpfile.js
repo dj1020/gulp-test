edf047ff52ddbfe3d2933bac88ebdc499c7fdd7b
Ken.Lin <ken.lin@cloud-interactive.com>
2014-12-03 15:28:17 +0800

    install plumber plugin to avoid gulp stopping watch files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 var gulp   = require('gulp');
 var uglify = require('gulp-uglify');
 var sass   = require('gulp-ruby-sass');
+var plumber = require('gulp-plumber');
 
 gulp.task('scripts', function () {
     gulp.src('js/*')
+        .pipe(plumber())
         .pipe(uglify())
         .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('styles', function () {
     gulp.src('sass/**/*.sass')
+        .pipe(plumber())
         .pipe(sass({
             style: 'compressed'
         }))
