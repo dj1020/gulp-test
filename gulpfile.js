4e8fb4da52a6aaa6b4e1680f97a4170834e2c786
Ken.Lin <ken.lin@cloud-interactive.com>
2014-12-04 15:43:10 +0800

    Yeah! 搞定！ 使用 gulp-connect 可以正常 livereload 了，而且超簡單~ perfect!

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
-// var cssmin = require('gulp-cssmin');
-var coffee = require('gulp-coffee');
-// var uglify = require('gulp-uglify');
-var jade   = require('gulp-jade');
+var gulp          = require('gulp');
+var connect       = require('gulp-connect');
+var sass          = require('gulp-ruby-sass');
+// var cssmin     = require('gulp-cssmin');
+var coffee        = require('gulp-coffee');
+// var uglify     = require('gulp-uglify');
+var jade          = require('gulp-jade');
 // var minifyHTML = require('gulp-minify-html');
 
 var paths = {
@@ -16,13 +17,20 @@ gulp.task('test', function () {
     console.log("TEST gulp");
 });
 
+gulp.task('webserver', function () {
+    connect.server({
+        livereload: true
+    });
+});
+
 gulp.task('styles', function () {
     gulp.src(paths.styles)
         .pipe(sass({
             // style: 'compressed'
         }))
         // .pipe(cssmin({keepSpecialComments: 0}))
-        .pipe(gulp.dest("css"));
+        .pipe(gulp.dest("css"))
+        .pipe(connect.reload());
 });
 
 gulp.task('scripts', function () {
@@ -47,4 +55,4 @@ gulp.task('watch', function() {
     gulp.watch(paths.jade, ['jade']);
 });
 
-gulp.task('default', ['styles', 'scripts', 'jade', 'watch']);
+gulp.task('default', ['webserver', 'styles', 'scripts', 'jade', 'watch']);
